55d28a6320a940ed464a704170ac7d11277447f9
lishiqiang1986 <419582581@qq.com>
2026-04-22 22:25:17 +0800

    首次上传任务清单小程序代码
    做这个小程序的初衷，就是希望带着购物清单去买东西的时候，不至于一个劲的手忙脚乱的操作手机，只要轻轻一点，就能完成操作，而且操作起来就跟男生玩游戏做任务一样，一目了然，还增加了分享功能，目前还有很多不完善的地方，还希望各位大佬，帮忙修改指正，本小程序已经发布到微信小程序上了，名字叫做“任务清单列表-DLBW"

---
+++ app_backup.js
@@ -0,0 +1,29 @@
+// app.js
+App({
+  onLaunch: function() {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserProfile 获取头像昵称
+          wx.getUserProfile({
+            desc: '用于完善用户资料',
+            success: res => {
+              console.log('获取用户信息成功:', res.userInfo)
+              // 可以将用户信息存储在全局变量中
+              this.globalData.userInfo = res.userInfo
+            },
+            fail: err => {
+              console.log('获取用户信息失败:', err)
+            }
+          })
+        }
+      }
+    })
+  },
+
+  globalData: {
+    userInfo: null,
+    hasUserInfo: false
+  }
+})
